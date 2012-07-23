38b755e7484939fd3de2c083aedc98ab9a4f1db5
yileiren <yileiren@126.coom>
2012-07-23 22:47:13 +0800

    提交。

--- YOrganization/OrgOperater.cs
+++ YOrganization/OrgOperater.cs
@@ -81,26 +81,32 @@ namespace YLR.YOrganization
                     }
                     else
                     {
-                        sql = string.Format("INSERT INTO org_organization (name,parentId) VALUES (%s,%d) SELECT SCOPE_IDENTITY() AS id"
+                        sql = string.Format("INSERT INTO org_organization (name,parentId) VALUES ('%s',%d) SELECT SCOPE_IDENTITY() AS id"
                             , org.name
                             , org.parentId);
                     }
 
                     //存入数据库
-                    this._orgDataBase.connectDataBase();
-                    DataTable retDt = this._orgDataBase.executeSqlReturnDt(sql);
-                    if (retDt != null && retDt.Rows.Count > 0)
+                    if (this._orgDataBase.connectDataBase())
                     {
-                        //获取组织机构id
-                        orgId = Convert.ToInt32(retDt.Rows[0]["id"]);
+                        DataTable retDt = this._orgDataBase.executeSqlReturnDt(sql);
+                        if (retDt != null && retDt.Rows.Count > 0)
+                        {
+                            //获取组织机构id
+                            orgId = Convert.ToInt32(retDt.Rows[0]["id"]);
+                        }
+                        else
+                        {
+                            this._errorMessage = "创建组织机构失败！";
+                            if (retDt == null)
+                            {
+                                this._errorMessage += "错误信息[" + this._orgDataBase.errorText + "]";
+                            }
+                        }
                     }
                     else
                     {
-                        this._errorMessage = "创建组织机构失败！";
-                        if (retDt == null)
-                        {
-                            this._errorMessage += "错误信息[" + this._orgDataBase.errorText + "]";
-                        }
+                        this._errorMessage = "连接数据库出错！错误信息[" + this._orgDataBase.errorText + "]";
                     }
                 }
             }
@@ -115,6 +121,92 @@ namespace YLR.YOrganization
             }
 
             return orgId;
+        }
+
+        /// <summary>
+        /// 根据登录名和密码获取用户信息，如果存在多个用户则返回id小的。
+        /// </summary>
+        /// <param name="logName">登陆名</param>
+        /// <param name="logPassword">密码</param>
+        /// <returns>成功返回用户，否则返回null。</returns>
+        public UserInfo getUser(string logName, string logPassword)
+        {
+            UserInfo user = null;
+            try
+            {
+                //构建SQL语句
+                string sql = string.Format("SELECT TOP(1) * FROM ORG_USER WHILE LOGNAME = '%s' AND LOGPASSWORD = '%s'",logName,logPassword);
+
+                //连接数据库
+                if (this._orgDataBase.connectDataBase())
+                {
+                    //获取用户
+                    DataTable dt = this._orgDataBase.executeSqlReturnDt(sql);
+
+                    //构建用户
+                    if (dt != null && dt.Rows.Count > 0)
+                    {
+                        user = new UserInfo();
+                        //获取用户id
+                        if (!dt.Rows[0].IsNull("ID"))
+                        {
+                            user.id = Convert.ToInt32(dt.Rows[0]["ID"]);
+
+                            //获取用户登录名和密码
+                            user.logName = logName;
+                            user.logPassword = logPassword;
+
+                            //用户姓名
+                            if (!dt.Rows[0].IsNull("NAME"))
+                            {
+                                user.name = Convert.ToString(dt.Rows[0]["NAME"]);
+                            }
+
+                            //用户所属组织机构
+                            if (!dt.Rows[0].IsNull("ORGANIZATIONID"))
+                            {
+                                user.organizationId = Convert.ToInt32(dt.Rows[0]["ORGANIZATIONID"]);
+                            }
+
+                            //是否删除
+                            if (!dt.Rows[0].IsNull("ISDELETE"))
+                            {
+                                if ("Y" == dt.Rows[0]["ISDELETE"].ToString())
+                                {
+                                    user.isDelete = true;
+                                }
+                                else
+                                {
+                                    user.isDelete = false;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        this._errorMessage = "获取用户信息出错！";
+
+                        if (dt == null)
+                        {
+                            this._errorMessage += "错误信息[" + this._orgDataBase.errorText + "]";
+                        }
+                    }
+                }
+                else
+                {
+                    this._errorMessage = "连接数据库出错！错误信息[" + this._orgDataBase.errorText +"]";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                this._orgDataBase.disconnectDataBase();
+            }
+
+            return user;
         }
     }
 }
