40e4b4243a02e6468f53834cc17d530273a23bf9
yileiren <yileiren@126.coom>
2012-08-03 17:59:02 +0800

    增加菜单操作类。

--- YMenu/MenuInfo.cs
+++ YMenu/MenuInfo.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace YMenu
+namespace YLR.YMenu
 {
     /// <summary>
     /// 菜单实体类。
---
+++ YMenu/MenuOperater.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YLR.YAdoNet;
+
+namespace YLR.YMenu
+{
+    /// <summary>
+    /// 菜单操作类库封装。
+    /// </summary>
+    public class MenuOperater
+    {
+        /// <summary>
+        /// 菜单数据库实例。
+        /// </summary>
+        protected YDataBase _menuDataBase = null;
+
+        /// <summary>
+        /// 菜单数据库实例。
+        /// </summary>
+        public YDataBase menuDataBase
+        {
+            set
+            {
+                this._menuDataBase = value;
+            }
+            get
+            {
+                return this._menuDataBase;
+            }
+        }
+
+        /// <summary>
+        /// 错误信息。
+        /// </summary>
+        protected string _errorMessage = "";
+
+        /// <summary>
+        /// 错误信息。
+        /// </summary>
+        public string errorMessage
+        {
+            get
+            {
+                return this._errorMessage;
+            }
+        }
+    }
+}
