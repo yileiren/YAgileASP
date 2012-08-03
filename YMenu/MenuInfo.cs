4fa41232cd00eb2042f4bff94200f6a7a8601210
yileiren <yileiren@126.coom>
2012-08-03 17:36:08 +0800

    添加菜单操作类库项目。

---
+++ YMenu/MenuInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YMenu
+{
+    /// <summary>
+    /// 菜单实体类。
+    /// </summary>
+    public class MenuInfo
+    {
+    }
+}
